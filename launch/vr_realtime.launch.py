6a540439d1b176be46763fe8b41f0585edce146b
PFE25 <marc-arthur.georges.1@ens.etsmtl.ca>
2025-07-11 13:22:38 -0400

    Changements apportés au package vr_unity_ros_doosan

--- launch/vr_realtime.launch.py
+++ launch/vr_realtime.launch.py
@@ -1,40 +1,31 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import FindPackageShare
+from launch_ros.actions import Node
 import os
 
 def generate_launch_description():
-    # Path to the Realsense launch file
-    realsense_launch_path = os.path.join(
-        FindPackageShare('realsense2_camera').find('realsense2_camera'),
-        'launch',
-        'rs_camera.launch.py'  # A voir quel est le nom du launch file
-    )
 
     return LaunchDescription([
-        # Conversion twist to dsr node
+        
         Node(
             package='vr_unity_ros_doosan',
-            executable='unity_dsr_twist.py',
+            executable='unity_dsr_twist', 
             name='unity_dsr_twist',
             output='screen'
         ),
+
+        Node(
+            package='vr_unity_ros_doosan',
+            executable='unity_dsr_jointspeed',
+            name='unity_dsr_jointspeed',
+            output='screen'  
+        ),
     
-        # # Realsense launch include
-        # IncludeLaunchDescription(
-        #     PythonLaunchDescriptionSource(realsense_launch_path),
-        #     launch_arguments=[
-        #         ('filters', 'pointcloud'),
-        #         ('color_width', '424'),
-        #         ('depth_width', '424'),
-        #         ('infra_width', '424'),
-        #         ('color_height', '240'),
-        #         ('depth_height', '240'),
-        #         ('infra_height', '240'),
-        #         ('color_fps', '30'),
-        #         ('depth_fps', '30'),
-        #         ('infra_fps', '30')
-        #     ]
-        # ),
+        Node(
+            package='ros_tcp_endpoint',
+            executable='default_server_endpoint',
+            name='default_server_endpoint',
+            output='screen',
+            parameters=[{'ROS_IP': '0.0.0.0', 'ROS_TCP_PORT': 10000}]
+        ),
+
     ])
--- setup.py
+++ setup.py
@@ -7,24 +7,25 @@ package_name = 'vr_unity_ros_doosan'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-        ['resource/' + package_name]),
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='coro',
-    maintainer_email='   TODO   ',
-    description='unity_ros_doosan package',
+    maintainer_email='TODO@example.com',
+    description='Unity-ROS2 bridge for Doosan robot integration.',
     license='TODO',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            
+            'unity_dsr_twist = vr_unity_ros_doosan.unity_dsr_twist:main',
+            'unity_dsr_jointspeed = vr_unity_ros_doosan.unity_dsr_jointspeed:main',
         ],
     },
 )
